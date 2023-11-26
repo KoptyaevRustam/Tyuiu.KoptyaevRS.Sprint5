40783663d74e406f792d13559580d6fd877ca7f6
Tyuiu.KoptyaevRS@outlook.com <rustam.koptyaev@mail.ru>
2023-11-26 20:06:19 +0500

    Task5

---
+++ Tyuiu.KoptyaevRS.Sprint5.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KoptyaevRS.Sprint5.Task5.V14.Lib
+{
+    public class DataService : ISprint5Task5V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 1;
+            double min = -100000;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if ((Convert.ToDouble(line) % 3 == 0))
+                    {
+                        if (min < Convert.ToDouble(line))
+                        {
+                            min = Convert.ToDouble(line);
+                        }
+                    }
+                }
+            }
+            for (int i = 1; i <= min; i++)
+            {
+                res = res * i;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KoptyaevRS.Sprint5.Task5.V14/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KoptyaevRS.Sprint5.Task5.V14.Lib;
+
+namespace Tyuiu.KoptyaevRS.Sprint5.Task5.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Коптяев Р. С. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Коптяев Рустам Сергеевич | АСОиУБ-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти факториал наибольшего целого числа, которое делится на 3, в       *");
+            Console.WriteLine("* файле. Полученный результат вывести на консоль. У вещественных значений *");
+            Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V14.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("Факториал наибольшего целого числа = " + res);
+            Console.ReadKey();
+        }
+    }
+}
